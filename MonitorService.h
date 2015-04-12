41cbfe8fba10d6e2d6581bb3f71bb2670e5154bf
kalamkar <kalamkar@localhost>
2015-04-12 19:56:19 +0000

    Got notify peak percent and readable 512 bytes data characteristics working

--- MonitorService.h
+++ MonitorService.h
@@ -8,35 +8,55 @@
 class MonitorService {
 public:
 
+    static const unsigned SENSOR_MAX        = 1024;
+    
     static const unsigned UUID_SERVICE      = 0x1901;
-    static const unsigned UUID_CHAR         = 0x1902;    
+    static const unsigned UUID_CHAR_DATA    = 0x1902;
+    static const unsigned UUID_CHAR_PEAK    = 0x1903;
     static const uint16_t MAX_DATA_LEN      = 512;
 
     MonitorService(BLEDevice &_ble) :
         ble(_ble),
         length(0),
-        monitor(UUID_CHAR, data, length, MAX_DATA_LEN,
-                GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY) {
-        GattCharacteristic *charTable[] = {&monitor};
-        GattService         monitorService(MonitorService::UUID_SERVICE, charTable, 1);
+        sampleMax(0),
+        sensorData(UUID_CHAR_DATA, data, 0, MAX_DATA_LEN,
+                    GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_READ),
+        peak(UUID_CHAR_PEAK, &peakPercent, sizeof(peakPercent), sizeof(peakPercent),
+                    GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY) {
+        GattCharacteristic *charTable[] = {&sensorData, &peak};
+        GattService         monitorService(MonitorService::UUID_SERVICE, charTable, 2);
         ble.addService(monitorService);
     }
     
     void addValue(uint16_t sample) {
-        data[length++] = sample >> 2;
+        buffer[length++] = sample >> 2;
+        sampleMax = sampleMax < sample ? sample : sampleMax;
+        
+        // Notify the variation value once buffer is full
         if (length == MAX_DATA_LEN) {
-            ble.updateCharacteristicValue(monitor.getValueHandle(), data, length);   
+            peakPercent = sampleMax * 100 / SENSOR_MAX;
+            ble.updateCharacteristicValue(peak.getValueHandle(), &peakPercent, sizeof(peakPercent));
+            
+            memcpy(data, buffer, MAX_DATA_LEN);
+            ble.updateCharacteristicValue(sensorData.getValueHandle(), data, MAX_DATA_LEN);
+            
             length = 0;
+            sampleMax = 0;
         }
     }
 
 private:
     BLEDevice           &ble;
 
-    uint8_t             data[MAX_DATA_LEN];
+    uint8_t             buffer[MAX_DATA_LEN];
     uint16_t            length;
+    uint8_t             data[MAX_DATA_LEN];
+    
+    uint8_t             peakPercent;
+    uint16_t            sampleMax;
 
-    GattCharacteristic  monitor;
+    GattCharacteristic  sensorData;
+    GattCharacteristic  peak;
 };
 
 #endif /* #ifndef __MONITOR_SERVICE_H__*/
